836b62f7b87c4878d8157c6bfded4e78c289cc66
Gulbeyaz <glbzbayram@gmail.com>
2023-10-09 18:00:02 +0300

    Main was created for connecting all components

---
+++ web-sayfam/src/layouts/Main.js
@@ -0,0 +1,13 @@
+import Mode from "../components/Mode";
+import Header from "./Header";
+import PageContents from "./PageContents";
+
+export default function Main() {
+  return (
+    <>
+      <Mode />
+      <Header />
+      <PageContents />
+    </>
+  );
+}
