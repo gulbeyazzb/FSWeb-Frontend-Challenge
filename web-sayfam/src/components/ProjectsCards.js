989c47cccc7d3d81995c38051ee74d42fba79349
Gulbeyaz <glbzbayram@gmail.com>
2023-10-10 00:40:39 +0300

    ProjectCards component was coverted to dynamic

--- web-sayfam/src/components/ProjectsCards.js
+++ web-sayfam/src/components/ProjectsCards.js
@@ -4,44 +4,44 @@ import {
   CardBody,
   CardFooter,
   Typography,
-  Button,
 } from "@material-tailwind/react";
+import { Data } from "../Data";
 
 const ProjectsCards = () => {
+  const changeImgSize = (e) => {};
   return (
-    <Card className="mt-6 w-96">
-      <CardHeader color="blue-gray" className="relative h-56">
-        <img
-          src="https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-          alt="card-image"
-        />
-      </CardHeader>
-      <CardBody>
-        <Typography
-          variant="h5"
-          color="blue-gray"
-          className="font-medium text-3xl dark:text-[#CFCBFF] pb-6 mb-2"
-        >
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          A simple, customizable, minimal setup cookie plugin that allows your
-          users to select which cookies to accept or decline. This was created
-          with vanilla JS, SCSS and Parcel Bundler and is available as a NPM
-          package and the git repository makes any type of customization to code
-          and themes possible.
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <button
-          className="flex items-center justify-center gap-2 text-[#3730A3] dark:bg-[#383838]
+    <div className="flex gap-6 items-center justify-center">
+      {Data.map((data) => (
+        <Card className="mt-6 w-96">
+          <CardHeader color="blue-gray" className="relative h-56">
+            <button onClick={changeImgSize}>
+              <img id="a" src={data.image} alt="card-image" />
+            </button>
+          </CardHeader>
+          <CardBody>
+            <Typography
+              variant="h5"
+              color="blue-gray"
+              className="font-medium text-3xl dark:text-[#CFCBFF] pb-6 mb-2"
+            >
+              {data.projectHeader}
+            </Typography>
+            <Typography>{data.introduction}</Typography>
+          </CardBody>
+          <CardFooter className="flex gap-3 pt-0">
+            {data.techStack.map((d) => (
+              <p
+                className="flex items-center justify-center gap-2 text-[#3730A3] dark:bg-[#383838]
           dark:text-[#E1E1FF] text-sm font-medium py-2 px-1 rounded-md w-[131px]
           h-12"
-        >
-          react
-        </button>
-      </CardFooter>
-    </Card>
+              >
+                {d}
+              </p>
+            ))}
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
   );
 };
 export default ProjectsCards;
