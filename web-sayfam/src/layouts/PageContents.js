72e7d49098740ac4747d0fc4ab0043da0125c2da
Gulbeyaz <glbzbayram@gmail.com>
2023-10-09 17:59:11 +0300

    PageContents was created and all pages were connected

---
+++ web-sayfam/src/layouts/PageContents.js
@@ -0,0 +1,13 @@
+import Hero from "../pages/Hero";
+import Skills from "../pages/Skills";
+import Profile from "../pages/Profile";
+
+export default function PageContents() {
+  return (
+    <>
+      <Hero />
+      <Skills />
+      <Profile />
+    </>
+  );
+}
