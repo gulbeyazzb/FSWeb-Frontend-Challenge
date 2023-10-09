b4eb695d1b4ac9aeefdc1c39e1893835fd64d10c
Gulbeyaz <glbzbayram@gmail.com>
2023-10-09 17:58:16 +0300

    All components were collected in Main.js

--- web-sayfam/src/App.js
+++ web-sayfam/src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import { useState } from "react";
-import Header from "./components/Header";
+import Main from "./layouts/Main";
 
 function App() {
   return (
-    <div className="container">
-      <Header />
+    <div className="flex flex-col pt-6 px-20 dark:bg-[#252128] h-full">
+      <Main />
     </div>
   );
 }
