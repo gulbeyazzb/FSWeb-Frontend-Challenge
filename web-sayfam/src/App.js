8c06650b81fe9b0dade32dc7614169747c997877
Gulbeyaz <glbzbayram@gmail.com>
2023-10-13 02:11:15 +0300

    font-[inter] was added and deleted

--- web-sayfam/src/App.js
+++ web-sayfam/src/App.js
@@ -5,7 +5,7 @@ import Main from "./layouts/Main";
 function App() {
   return (
     <div>
-      <div className="flex flex-col pt-6 px-20 dark:bg-[#252128] h-full">
+      <div className=" flex flex-col pt-6 px-20 dark:bg-[#252128] h-full">
         <Main />
       </div>
 
