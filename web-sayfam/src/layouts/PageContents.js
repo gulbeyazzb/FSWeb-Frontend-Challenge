ba02cfbece8b271efbb1fb53d94f8dfb291b9dc5
Gulbeyaz <glbzbayram@gmail.com>
2023-10-13 02:08:05 +0300

    Mo component was imported but then it was deleted

--- web-sayfam/src/layouts/PageContents.js
+++ web-sayfam/src/layouts/PageContents.js
@@ -2,6 +2,7 @@ import Hero from "../pages/Hero";
 import Skills from "../pages/Skills";
 import Profile from "../pages/Profile";
 import Projects from "../pages/Projects";
+import { Mo } from "../components/Mo";
 export default function PageContents() {
   return (
     <>
