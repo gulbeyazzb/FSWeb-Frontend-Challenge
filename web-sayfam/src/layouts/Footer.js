50147635fef871024a834b0a4ad0303298c364fa
Gulbeyaz <glbzbayram@gmail.com>
2023-10-13 02:10:46 +0300

    mail address was defined as mailTo href

--- web-sayfam/src/layouts/Footer.js
+++ web-sayfam/src/layouts/Footer.js
@@ -4,33 +4,35 @@ import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 export default function Footer() {
   return (
     <div className="dark:bg-[#141414] m-auto p-[5.5rem]  ">
-      <h1 className="dark:text-[#AEBCCF] font-semibold  text-[42px] pb-6 mb-6 w-[500px]">
+      <h1 className="dark:text-[#AEBCCF] font-semibold text-[#1F2937] text-[42px] pb-6 mb-6 w-[500px]">
         Let’s work together on your next product.
       </h1>
       <div className="flex justify-between">
         <a
-          href="https://mail.google.com/mail/u/0/?hl=tr#inbox?compose=new"
+          href="mailto:glbzbayram@gmail.com"
           target="_blank"
-          className="dark:text-[#BAB2E7] font-medium text-xl"
+          className="dark:text-[#BAB2E7] text-[#AF0C48] font-medium text-xl"
         >
           👉 glbzbayram@gmail.com
         </a>
         <div className="flex justify-between gap-6">
           <a
             href="http://localhost:3000/"
-            className="dark:text-[#E1E1FF] font-medium text-lg"
+            className="dark:text-[#E1E1FF] text-[#0A0A14] font-medium text-lg"
           >
             Personal Blog
           </a>
           <a
             href="https://github.com/gulbeyazzb"
-            className="dark:text-[#17D18B] font-medium text-lg"
+            className="text-[#17D18B] font-medium text-lg"
+            target="_blank"
           >
             Github
           </a>
           <a
             href="https://www.linkedin.com/in/g%C3%BClbeyaz-bayram-%C3%B6zer-4a6454159/"
-            className="dark:text-[#0BA6F6] font-medium text-lg"
+            className="text-[#0BA6F6] font-medium text-lg"
+            target="_blank"
           >
             Linkedin
           </a>
