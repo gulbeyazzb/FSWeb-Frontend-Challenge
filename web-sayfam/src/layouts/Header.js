dee9aef203903c30a3ddd1d79feade0978cabeb5
Gulbeyaz <glbzbayram@gmail.com>
2023-10-13 02:09:55 +0300

    editted

--- web-sayfam/src/layouts/Header.js
+++ web-sayfam/src/layouts/Header.js
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 const Header = () => {
   return (
     <div className="flex justify-between py-10">
-      <div className="w-[63.68px] h-[62px] font-semibold text-2xl p-3 text-center bg-[#EEEBFF] text-[#7B61FF] dark:bg-[#4731D3] border-none rounded-full dark:text-[#8F88FF]">
+      <div className="w-[63.68px] h-[62px] font-semibold text-2xl p-3  text-center bg-[#EEEBFF] text-[#7B61FF] dark:bg-[#4731D3] border-none rounded-full dark:text-[#8F88FF]">
         G
       </div>
       <Navbar />
