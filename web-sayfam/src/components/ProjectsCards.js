41e3f5c8d0cf94fb23ab25c0def41a937e3a926e
Gulbeyaz <glbzbayram@gmail.com>
2023-10-13 02:09:39 +0300

    Modal css was added to Projects images

--- web-sayfam/src/components/ProjectsCards.js
+++ web-sayfam/src/components/ProjectsCards.js
@@ -5,18 +5,37 @@ import {
   CardFooter,
   Typography,
 } from "@material-tailwind/react";
-import { Data } from "../Data";
+import { Data } from "../mocks/Data";
+import { useState } from "react";
 
 const ProjectsCards = () => {
-  const changeImgSize = (e) => {};
+  const [showModal, setShowModal] = useState(false);
+  const showNextImage = () => {};
   return (
-    <div className="flex gap-6 items-center justify-center">
+    <div className="flex container m-auto gap-6 items-center justify-between">
       {Data.map((data) => (
-        <Card className="mt-6 w-96">
-          <CardHeader color="blue-gray" className="relative h-56">
-            <button onClick={changeImgSize}>
-              <img id="a" src={data.image} alt="card-image" />
+        <Card className="mt-6 w-80 py-10 pt-12 ">
+          <CardHeader className="relative ">
+            <button onClick={() => setShowModal(true)}>
+              <img src={data.image} alt="card-image" />
             </button>
+
+            {showModal && (
+              <button
+                className="text-danger-700 text-lg e font-medium"
+                onClick={() => setShowModal(false)}
+              >
+                <div className=" fixed inset-0 bg-black bg-opacity-90 backdrop-blur-sm flex flex-col justify-center items-center">
+                  <div className="text-right">
+                    <img
+                      src={data.image}
+                      alt="card-image"
+                      className="w-[90%] m-auto"
+                    />
+                  </div>
+                </div>
+              </button>
+            )}
           </CardHeader>
           <CardBody>
             <Typography
@@ -29,16 +48,23 @@ const ProjectsCards = () => {
             <Typography>{data.introduction}</Typography>
           </CardBody>
           <CardFooter className="flex gap-3 pt-0">
-            {data.techStack.map((d) => (
-              <p
-                className="flex items-center justify-center gap-2 text-[#3730A3] dark:bg-[#383838]
-          dark:text-[#E1E1FF] text-sm font-medium py-2 px-1 rounded-md w-[131px]
-          h-12"
-              >
-                {d}
+            {data.techStack.map((tech) => (
+              <p className="border-solid border border-[#3730A3] flex items-center gap-2 text-[#3730A3] dark:bg-[#383838] dark:text-[#E1E1FF] w-[131px] dark:border-[#E1E1FF] text-sm font-medium pl-5 rounded-md  h-12">
+                {tech}
               </p>
             ))}
           </CardFooter>
+          <div className="flex justify-between px-8  text-[#3730A3] underline">
+            <a href="https://github.com/gulbeyazzb" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://www.linkedin.com/in/g%C3%BClbeyaz-bayram-%C3%B6zer-4a6454159/"
+              target="_blank"
+            >
+              Website
+            </a>
+          </div>
         </Card>
       ))}
     </div>
