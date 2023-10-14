ee1c077edef9f5e7c2ab1a6fac56a302dfb66aa0
Gulbeyaz <glbzbayram@gmail.com>
2023-10-15 01:31:02 +0300

    Tr files were created to change language

---
+++ web-sayfam/src/mocks/TrProfileData.js
@@ -0,0 +1,15 @@
+export const Data = {
+  birthDate: " 3 Mart 1993",
+  city: " Konya,Türkiye",
+  education:
+    " Mehmet Akif Ersoy Üniversitesi, Bilgisayar Mühendisliği Lisans (3.75), 2020",
+  role: [
+    " Full Stack Developer,",
+    " Front End Developer,",
+    " Back End Developer",
+  ],
+  about: [
+    "Yeni beceriler kazanmak ve yeni zorluklarla kendimi sürekli geliştirmek için büyüme zihniyetine sahip, hevesli bir tam kapsamlı geliştiriciyim.",
+    "4 yıllık bilgisayar mühendisliği eğitimimin ardından kendimi çok daha iyi bir versiyona dönüştürmek ve eksik gördüğüm yazılım becerilerimde kendimi geliştirmek amacıyla 6 aylık yoğun Workintech full-stack geliştirme programını tamamladım ve Ön uç, arka uç ve CS'de iyi beceriler kazandım.",
+  ],
+};
---
+++ web-sayfam/src/mocks/TrProjectsData.js
@@ -0,0 +1,35 @@
+export const Data = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80",
+    projectHeader: "UI/UX Review Check",
+    introduction:
+      " Kullanıcılarınızın hangi çerezleri kabul edeceğini veya reddedeceğini seçmesine olanak tanıyan basit, özelleştirilebilir, minimal kurulum çerez eklentisi. Bu, vanilya JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu, kod ve temalarda her türlü özelleştirmeyi mümkün kılar.",
+    techStack: ["react", "Redux", "Axios"],
+    github: "https://github.com/gulbeyazzb",
+    website:
+      "https://www.linkedin.com/in/g%C3%BClbeyaz-bayram-%C3%B6zer-4a6454159/",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80",
+    projectHeader: "UI/UX Review Check",
+    introduction:
+      " Kullanıcılarınızın hangi çerezleri kabul edeceğini veya reddedeceğini seçmesine olanak tanıyan basit, özelleştirilebilir, minimal kurulum çerez eklentisi. Bu, vanilya JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu, kod ve temalarda her türlü özelleştirmeyi mümkün kılar.",
+    techStack: ["react", "Redux", "Axios"],
+    github: "https://github.com/gulbeyazzb",
+    website:
+      "https://www.linkedin.com/in/g%C3%BClbeyaz-bayram-%C3%B6zer-4a6454159/",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1540553016722-983e48a2cd10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80",
+    projectHeader: "UI/UX Review Check",
+    introduction:
+      " Kullanıcılarınızın hangi çerezleri kabul edeceğini veya reddedeceğini seçmesine olanak tanıyan basit, özelleştirilebilir, minimal kurulum çerez eklentisi. Bu, vanilya JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur ve git deposu, kod ve temalarda her türlü özelleştirmeyi mümkün kılar.",
+    techStack: ["react", "Redux", "Axios"],
+    github: "https://github.com/gulbeyazzb",
+    website:
+      "https://www.linkedin.com/in/g%C3%BClbeyaz-bayram-%C3%B6zer-4a6454159/",
+  },
+];
