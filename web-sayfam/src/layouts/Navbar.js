0143dc0231e85c4cc57ce92a7e440de6e59045f7
Gulbeyaz <glbzbayram@gmail.com>
2023-10-16 12:52:44 +0300

    navbar links were scrolled using onClick

--- web-sayfam/src/layouts/Navbar.js
+++ web-sayfam/src/layouts/Navbar.js
@@ -1,20 +1,40 @@
-import { useState } from "react";
+import { animateScroll as scroll } from "react-scroll";
 
 export default function Navbar() {
+  const scrollTo = (px) => {
+    scroll.scrollTo(px); // Scrolling to 100px from the top of the page.
+  };
+
+  const scrollToBottom = () => {
+    scroll.scrollToBottom();
+  };
+
   return (
     <div className="flex md:justify-between justify-around w-96 items-center">
-      <a href="" className="text-[#6B7280] font-medium text-lg">
+      <a
+        onClick={() => scrollTo(550)}
+        className="text-[#6B7280] font-medium text-lg cursor-pointer"
+      >
         Skills
       </a>
-      <button className="text-center md:hidden text-[#3730A3] bg-white border-solid border border-[#3730A3] dark:border-none rounded-md font-medium text-lg  w-[120px] h-10">
+      <a
+        onClick={scrollToBottom}
+        className="cursor-pointer text-center md:hidden text-[#3730A3] bg-white border-solid border border-[#3730A3] dark:border-none rounded-md font-medium text-lg  w-[120px] h-10"
+      >
         Hire Me
-      </button>
-      <a href="" className="text-[#6B7280] font-medium text-lg">
+      </a>
+      <a
+        onClick={() => scrollTo(1600)}
+        className="text-[#6B7280] font-medium text-lg cursor-pointer"
+      >
         Projects
       </a>
-      <button className="text-center hidden md:inline-block text-[#3730A3] bg-white border-solid border-2 border-[#3730A3] dark:border-none rounded-md font-medium text-lg  w-[131px] h-12">
+      <a
+        onClick={scrollToBottom}
+        className="cursor-pointer md:py-3 py-4 px-6 md:px-8 text-center hidden md:inline-block text-[#3730A3] bg-white border-solid border-2 border-[#3730A3] dark:border-none rounded-md font-medium text-lg h-12"
+      >
         Hire Me
-      </button>
+      </a>
     </div>
   );
 }
