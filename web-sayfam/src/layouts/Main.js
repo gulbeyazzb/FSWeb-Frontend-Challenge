29fa46cc0d8d3bab2a9243547e77b56dd0be8d0a
Gulbeyaz <glbzbayram@gmail.com>
2023-10-16 12:52:55 +0300

    edited

--- web-sayfam/src/layouts/Main.js
+++ web-sayfam/src/layouts/Main.js
@@ -1,13 +1,8 @@
-import Mode from "../components/Mode";
-import Footer from "./Footer";
-import Header from "./Header";
 import PageContents from "./PageContents";
 
 export default function Main() {
   return (
     <>
-      <Mode />
-      <Header />
       <PageContents />
     </>
   );
