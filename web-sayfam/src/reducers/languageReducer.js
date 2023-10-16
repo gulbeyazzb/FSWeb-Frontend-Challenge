c0132ce2cda186ca4996ce82a2f1e8d3ebbdd48d
Gulbeyaz <glbzbayram@gmail.com>
2023-10-16 23:04:10 +0300

    reducer was created and  switch case defined

--- web-sayfam/src/reducers/languageReducer.js
+++ web-sayfam/src/reducers/languageReducer.js
@@ -1,8 +1,25 @@
 import { TR } from "../actions/languageAction";
 import { EN } from "../actions/languageAction";
+import { TrHeroData } from "../mocks/TrHeroData";
+import { TrProfileData } from "../mocks/TrProfileData";
+import { TrProjectsData } from "../mocks/TrProjectsData";
+import { TrSkillsData } from "../mocks/TrSkillsData";
+import { TrNavBarData } from "../mocks/TrNavBarData";
+import { TrFooterData } from "../mocks/TrFooterData";
+import { SkillsData } from "../mocks/SkillsData";
+import { HeroData } from "../mocks/HeroData";
+import { ProfileData } from "../mocks/ProfileData";
+import { ProjectsData } from "../mocks/ProjectsData";
+import { NavBarData } from "../mocks/NavBarData";
+import { FooterData } from "../mocks/FooterData";
 
 const stateInitial = {
-  data: "",
+  heroData: HeroData,
+  profileData: ProfileData,
+  projectsData: ProjectsData,
+  skillsData: SkillsData,
+  navBarData: NavBarData,
+  footerData: FooterData,
 };
 
 const languageReducer = (state = stateInitial, action) => {
@@ -11,6 +28,23 @@ const languageReducer = (state = stateInitial, action) => {
     case TR:
       return {
         ...state,
+        heroData: TrHeroData,
+        profileData: TrProfileData,
+        projectsData: TrProjectsData,
+        skillsData: TrSkillsData,
+        navBarData: TrNavBarData,
+        footerData: TrFooterData,
+      };
+
+    case EN:
+      return {
+        ...state,
+        heroData: HeroData,
+        profileData: ProfileData,
+        projectsData: ProjectsData,
+        skillsData: SkillsData,
+        navBarData: NavBarData,
+        footerData: FooterData,
       };
     default:
       return state;
