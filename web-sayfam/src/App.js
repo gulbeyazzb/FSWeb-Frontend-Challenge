c912dd7988f7063da2f9634196fc07a45ba400ad
Gulbeyaz <glbzbayram@gmail.com>
2023-10-16 12:51:33 +0300

    arrow top icon added app.js and added scrolling top func

--- web-sayfam/src/App.js
+++ web-sayfam/src/App.js
@@ -1,15 +1,31 @@
 import "./App.css";
 import Footer from "./layouts/Footer";
 import Main from "./layouts/Main";
+import { animateScroll as scroll } from "react-scroll";
+import Mode from "./components/Mode";
+import Header from "./layouts/Header";
 
 function App() {
+  const scrollToTop = () => {
+    scroll.scrollToTop();
+  };
   return (
-    <div className="dark:bg-[#252128]">
-      <div className=" flex flex-col pt-6 sm:px-20 px-2 h-full md:w-[1100px] m-auto">
+    <div className=" dark:bg-[#252128]">
+      <div className=" flex flex-col pt-6 sm:px-20 px-2 md:w-[1100px] md:m-auto">
+        <Mode />
+        <Header />
+      </div>
+      <div className=" flex flex-col pt-6 sm:px-20 px-2 h-full md:w-[1100px] md:m-auto ">
         <Main />
       </div>
 
       <Footer />
+      <a
+        onClick={scrollToTop}
+        className="fixed right-10 bottom-2 rounded-[100%]"
+      >
+        <i class="bx bx-up-arrow-circle text-[#7673a6] text-4xl hover:text-[#3730A3]"></i>
+      </a>
     </div>
   );
 }
