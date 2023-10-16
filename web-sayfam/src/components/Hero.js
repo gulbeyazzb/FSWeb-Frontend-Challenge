52508121f2fa3446b24a3640bd994eba7101c955
Gulbeyaz <glbzbayram@gmail.com>
2023-10-16 23:08:52 +0300

    heroData was pulled according to selected language

--- web-sayfam/src/components/Hero.js
+++ web-sayfam/src/components/Hero.js
@@ -1,39 +1,43 @@
 import line from "../Assets/Line 11.png";
 import mePhoto from "../Assets/9C6BB119-4D9F-45F0-B1FD-9E332E9EF7BA-35928-0000062DD430959D (1).jpg";
-import { Data } from "../mocks/HeroData";
+// import { HeroData } from "../mocks/HeroData";
 import { Link } from "react-router-dom";
 import { animateScroll as scroll } from "react-scroll";
+import { useSelector } from "react-redux";
 
 export default function Hero() {
   const scrollToBottom = () => {
     scroll.scrollToBottom();
   };
-  console.log(Data.name);
+  const HeroData = useSelector((store) => store.heroData);
+
   return (
     <div>
-      <div className="flex items-center gap-20">
+      <div className="md:flex md:items-center gap-20">
         <div>
           <div className="flex justify-center md:justify-start md:items-center md:gap-2 pb-6">
             <img src={line} className="hidden md:inline-block"></img>
             <p className="text-navy-blue-600 dark:text-navy-blue-100 font-medium text-xl ">
-              {Data.name}
-              <p className="text-center">{Data.role}</p>
+              {HeroData.name}
+              <p className="text-center">{HeroData.role}</p>
             </p>
           </div>
+          <img
+            className="md:w-[300px] w-52 md:float-right m-auto rounded-md"
+            src={mePhoto}
+          ></img>
           <h2 className="dark:text-[#AEBCCF] text-[#1F2937] md:font-bold md:text-7xl text-xl font-bold md:pb-6 ">
-            {Data.about}
+            {HeroData.aboutHeader}
           </h2>
           <p className="dark:text-white text-[#6B7280] font-normal text-lg md:w-[553px]  pb-6">
-            Hi, I’m Almila. I’m a full-stack developer. If you are looking for a
-            Developer who to craft solid and scalable frontend products with
-            great user experiences. Let’s shake hands with me.
+            {HeroData.aboutBody}
           </p>
           <div className="flex md:gap-2 items-center justify-evenly md:justify-start">
             <a
               onClick={scrollToBottom}
               className="hidden md:inline-block bg-[#3730A3] text-[#FFFFFF] dark:text-[#3730A3] dark:bg-[#E1E1FF] text-lg font-medium md:py-3 py-2 px-6 md:px-8  rounded-md h-12"
             >
-              Hire Me
+              Hire me
             </a>
             <a
               href="https://github.com/gulbeyazzb"
@@ -59,10 +63,6 @@ export default function Hero() {
             </a>
           </div>
         </div>
-        <img
-          className="md:w-[300px] w-52 md:float-right m-auto rounded-md"
-          src={mePhoto}
-        ></img>
       </div>
     </div>
   );
