9bd6c9114f14255817fdbe40816599232a479321
Gulbeyaz <glbzbayram@gmail.com>
2023-10-17 14:09:26 +0300

    darkMode was set to localStorage and get from localstorage

--- web-sayfam/src/components/Mode.js
+++ web-sayfam/src/components/Mode.js
@@ -1,19 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { enAction, trAction } from "../actions/languageAction";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 export default function Mode() {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") === "Light" ? "Dark" : "Light"
+  );
+
   const [toggle, setToggle] = useState(false);
-  const [dark, setDark] = useState(true);
-  const toggleClass = " transform translate-x-5 sm:translate-x-6";
   const dispatch = useDispatch();
   const [en, setEn] = useState(true);
 
-  const changeDarkMode = () => {
+  const toggleButtonClass = " transform translate-x-5 sm:translate-x-6";
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "Dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
+
+  const changeTheme = () => {
     setToggle(!toggle);
-    document.documentElement.classList.toggle("dark");
-    setDark(!dark);
+    setTheme(theme === "Light" ? "Dark" : "Light");
+    if (theme === "Dark") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "Dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "Light");
+    }
   };
+
   const changeLanguageMode = () => {
     en ? dispatch(trAction()) : dispatch(enAction());
     setEn(!en);
@@ -36,27 +60,27 @@ export default function Mode() {
         <div className="flex flex-col justify-center items-center ">
           <button
             className="md:w-14 md:h-7 w-12 h-6 flex items-center dark:bg-[#3A3A3A] bg-[#4731D3] rounded-full sm:p-1 cursor-pointer duration-700 ease-in-out "
-            onClick={changeDarkMode}
+            onClick={changeTheme}
           >
-            {!dark && (
+            {theme === "Dark" && (
               <i
                 class={
                   "bx bxs-moon text-[#FFE86E]  md:w-6 md:h-6 text-2xl sm:pb-8  transform  duration-700 ease-in-out " +
-                  (toggle ? null : toggleClass)
+                  (toggle ? null : toggleButtonClass)
                 }
               ></i>
             )}
-            {dark && (
+            {theme === "Light" && (
               <i
                 class={
                   " bx bxs-sun text-[#FFE86E]  md:w-6 md:h-6 text-2xl sm:pb-8  transform  duration-700 ease-in-out " +
-                  (toggle ? null : toggleClass)
+                  (toggle ? null : toggleButtonClass)
                 }
               ></i>
             )}
           </button>
         </div>
-        {dark ? "DarkMode" : "LightMode"}
+        {theme}Mode'a Geç
       </div>
     </div>
   );
