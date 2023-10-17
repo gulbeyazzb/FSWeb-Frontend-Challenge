994ffe7e094610066d90dfe534ec6e613d7df536
Gulbeyaz <glbzbayram@gmail.com>
2023-10-17 22:14:56 +0300

    contact page was created as form

---
+++ web-sayfam/src/pages/FormPage.js
@@ -0,0 +1,99 @@
+import { useForm } from "react-hook-form";
+import { Form, FormFeedback, FormGroup, Input, Label } from "reactstrap";
+import FormInput from "../components/atoms/FormInput";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+const FormPage = () => {
+  const push = useHistory();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      message: "",
+    },
+    mode: "onChange",
+  });
+
+  const onFormSubmit = (formData) => {
+    toast("Mesajınız başarıyla gönderiliyor");
+
+    setTimeout(() => {
+      push.push("/");
+    }, 2000);
+
+    axios
+      .post("olmayan-bir-url.com", formData)
+      .then((res) => {
+        localStorage.setItem("formData", JSON.stringify(formData));
+      })
+      .catch((res) => {
+        console.error(res);
+      });
+  };
+
+  return (
+    <div>
+      <Form
+        onSubmit={handleSubmit(onFormSubmit)}
+        className="m-auto w-full p-10 mb-6 rounded  bg-transparent shadow dark:shadow-white-md shadow-navy-blue-600 bg-white dark:bg-gray-300"
+      >
+        <FormGroup className="md:w-[500px] m-auto flex flex-col ">
+          <Label className="m-2 text-lg font-semibold">Name</Label>
+          <FormInput
+            className="p-2 m-2 border-2  shadow shadow-indigo-600"
+            type="email"
+            placeHolder="email@gmail.com"
+            register={register}
+            name={"email"}
+            validations={{
+              required: "Email is required!",
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                message: "Email is not valid.",
+              },
+            }}
+            invalid={!!errors.email?.message}
+            data-testid="email-input"
+          />
+          <FormFeedback className="text-deep-orange-900">
+            {errors.email?.message}
+          </FormFeedback>
+        </FormGroup>
+        <FormGroup className="md:w-[500px] m-auto flex flex-col">
+          <Label className="m-2 text-lg font-semibold">Message</Label>
+          <FormInput
+            className="p-2 m-2 border-2  shadow shadow-indigo-600"
+            type="text"
+            name="message"
+            invalid={!!errors.message?.message}
+            placeHolder="message"
+            register={register}
+            validations={{
+              required: "Message is required !",
+            }}
+          />
+          <FormFeedback className="text-deep-orange-900">
+            {errors.message?.message}
+          </FormFeedback>
+        </FormGroup>
+
+        <Input
+          className="cursor-pointer m-auto md:w-[300px] mt-6 px-6 py-2 shadow text-white bg-indigo-600 rounded-md flex justify-center items-center"
+          type="submit"
+          formNoValidate="formnovalidate"
+          value="Send Message"
+          disabled={false}
+          data-testid="login-submit-btn"
+        />
+        <ToastContainer />
+      </Form>
+    </div>
+  );
+};
+
+export default FormPage;
