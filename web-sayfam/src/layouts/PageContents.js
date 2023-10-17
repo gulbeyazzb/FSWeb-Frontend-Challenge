cf037c300a948e268a8495fa836cf9628925d6b6
Gulbeyaz <glbzbayram@gmail.com>
2023-10-17 22:15:08 +0300

    /contact route was defined

--- web-sayfam/src/layouts/PageContents.js
+++ web-sayfam/src/layouts/PageContents.js
@@ -5,11 +5,15 @@ import Profile from "../components/Profile";
 import { Switch } from "react-router-dom";
 import { Route } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
+import FormPage from "../pages/FormPage";
 export default function PageContents({ ref }) {
   return (
     <Switch>
       <Route path="/" exact>
         <HomePage />
+      </Route>
+      <Route path="/contact" exact>
+        <FormPage />
       </Route>
     </Switch>
   );
