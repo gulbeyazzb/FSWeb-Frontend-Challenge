4b33757c1612c8c5a56bb52df53248bf1f7e150b
Gulbeyaz <glbzbayram@gmail.com>
2023-10-17 22:16:49 +0300

    logo was defined as link go to homePage

--- web-sayfam/src/layouts/Header.js
+++ web-sayfam/src/layouts/Header.js
@@ -1,11 +1,15 @@
 import Navbar from "./Navbar";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="flex items-center md:justify-between py-10">
-      <div className="hidden md:inline-block w-12 md:w-[63.68px] md:h-[62px] font-semibold text-2xl md:p-3 p-2 text-center  bg-[#EEEBFF] text-[#7B61FF] dark:bg-[#4731D3] border-none rounded-full dark:text-[#8F88FF]">
+      <Link
+        to="/"
+        className="hidden md:inline-block w-12 md:w-[63.68px] md:h-[62px] font-semibold text-2xl md:p-3 p-2 text-center  bg-[#EEEBFF] text-[#7B61FF] dark:bg-[#4731D3] border-none rounded-full dark:text-[#8F88FF]"
+      >
         G
-      </div>
+      </Link>
       <Navbar />
     </div>
   );
