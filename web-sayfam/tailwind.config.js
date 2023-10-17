b86fb0c755968cf43fe53c26703fde188aae50f0
Gulbeyaz <glbzbayram@gmail.com>
2023-10-17 14:07:51 +0300

    darkMode class was added to tailwind.config file

--- web-sayfam/tailwind.config.js
+++ web-sayfam/tailwind.config.js
@@ -2,6 +2,7 @@
 const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
+  darkMode: "class",
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
