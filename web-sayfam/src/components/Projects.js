6a05e77f8414df7ded7712ab427231154d0e863d
Gulbeyaz <glbzbayram@gmail.com>
2023-10-19 17:23:28 +0300

    command line was deleted

--- web-sayfam/src/components/Projects.js
+++ web-sayfam/src/components/Projects.js
@@ -9,35 +9,6 @@ export default function Projects() {
         Projects
       </h2>
       <ProjectsCards />
-      {/* <div className="flex">
-        <div>
-          <h6 className="font-medium text-3xl dark:text-[#B7AAFF] pb-6">
-            JavaScript
-          </h6>
-          <p className="font-normal text-xs dark:text-white">
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
-        <div>
-          <h6 className="font-medium text-3xl dark:text-[#B7AAFF] pb-6">
-            React.Js
-          </h6>
-          <p className="font-normal text-xs dark:text-white">
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
-        <div>
-          <h6 className="font-medium text-3xl dark:text-[#B7AAFF] pb-6">
-            Node.Js
-          </h6>
-          <p className="font-normal text-xs dark:text-white">
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-            officia deserunt mollit anim id est laborum.
-          </p>
-        </div>
-      </div> */}
     </div>
   );
 }
