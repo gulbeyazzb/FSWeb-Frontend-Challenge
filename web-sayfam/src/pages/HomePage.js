4b170a09e8b070d04cb5d8d1617f3e651409ebad
Gulbeyaz <glbzbayram@gmail.com>
2023-10-19 17:23:13 +0300

    width was defined as global for homePage

--- web-sayfam/src/pages/HomePage.js
+++ web-sayfam/src/pages/HomePage.js
@@ -3,14 +3,20 @@ import Projects from "../components/Projects";
 import Hero from "../components/Hero";
 import Skills from "../components/Skills";
 import Profile from "../components/Profile";
+import Header from "../layouts/Header";
+import Footer from "../layouts/Footer";
 
 export const HomePage = () => {
   return (
     <>
-      <Hero />
-      <Skills />
-      <Profile />
-      <Projects />
+      <div className=" flex flex-col pt-6 sm:px-20 px-2 h-full md:w-[1100px] md:m-auto ">
+        <Header />
+        <Hero />
+        <Skills />
+        <Profile />
+        <Projects />
+      </div>
+      <Footer />
     </>
   );
 };
